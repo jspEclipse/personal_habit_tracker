ba4854463fb4e6186955fd93aee8c99455d03a7a
Jun Park <junpark1041@gmail.com>
2025-08-21 13:57:32 -0400

    fixed cursor issue with button, animated box shadow

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -14,7 +14,7 @@ export default function Login(){
           <div className="w-full flex flex-col">
             <label className="pb-1">Email</label>
             <input
-                className="w-full h-5 mb-3 rounded-xl border-2 border-white/10 text-white p-5 bg-white/5 backdrop-blur-none"
+                className="w-full h-5 mb-3 rounded-xl border-2 border-white/10 text-white p-5 bg-white/5 backdrop-blur-none focus:ring-2 shadow-gray-300 focus:outline-hidden transition-shadow duration-200 ease-in-out"
                 id="email"
                 type="email"
                 placeholder="m@example.com"
@@ -24,15 +24,15 @@ export default function Login(){
           <div className="w-full flex flex-col">
             <label className="pb-1">Password</label>
             <input 
-                className="w-full h-5 rounded-xl border-2 border-white/10 text-white p-5 bg-white/5 backdrop-blur-none"
+                className="w-full h-5 rounded-xl border-2 border-white/10 text-white p-5 bg-white/5 backdrop-blur-none focus:ring-2 shadow-gray-300 focus:outline-hidden transition-shadow duration-200 ease-in-out"
                 id="password" 
                 type="password" 
                 required />
           </div>
         </form>
         <div>
-          <div className="w-full h-5 rounded-xl border-2 mb-3 border-white/10 text-black p-5 bg-white/85 backdrop-blur-none flex justify-center items-center">Login</div>
-          <div className="w-full h-5 rounded-xl border-2 border-white/10 text-white p-5 bg-white/5 backdrop-blur-none flex justify-center items-center">Sign In</div>
+          <button className="cursor-pointer w-full h-5 rounded-xl border-2 mb-3 border-white/10 text-black p-5 hover:bg-white/85 bg-white transition-colors duration-200 backdrop-blur-none flex justify-center items-center">Login</button>
+          <button className="cursor-pointer w-full h-5 rounded-xl border-2 border-white/10 text-white p-5 bg-white/5 backdrop-blur-none flex justify-center items-center hover:bg-white/10 transition-colors duration-200">Sign In</button>
         </div>
       </div>
     );
